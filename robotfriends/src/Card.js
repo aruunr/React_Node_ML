ac893a73c64777eccb3dd575d6ba972cc464dfad
Arun Ramaswamy <aruunr@gmail.com>
2018-02-04 15:41:15 -0600

    deleted basic files for cleaning up

---
+++ robotfriends/src/Card.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const Card = () =>
+return (
+    <div>
+        <img alt = 'photo' src = ''>    
+    </div>
+    <div>
+        <h2>Arun </h2>
+        <p>arun@gmail.com</p>
+    </div>
+
+);
